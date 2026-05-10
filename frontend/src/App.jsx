2769ba7b6427b31bc8f8e309cdbfabbadddb0f9c
alastorhernan <alastorhernan@gmail.com>
2026-05-10 17:11:42 -0300

    Actualizacion automatica

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { EventService } from '../services/api'; 
+import { EventService } from './services/api'; 
 
 const Tripulantes = () => {
     const [tripulantes, setTripulantes] = useState([]);
